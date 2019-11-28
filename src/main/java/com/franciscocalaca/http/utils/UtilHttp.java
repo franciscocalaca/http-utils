f9f4ae5eeff77172dc9395e1f9eb24e8fd7a37d1
Francisco Calaca <chicocx@gmail.com>
2019-11-28 17:25:38 -0200

    suporte a proxy

--- src/main/java/com/franciscocalaca/http/utils/UtilHttp.java
+++ src/main/java/com/franciscocalaca/http/utils/UtilHttp.java
@@ -6,7 +6,11 @@ import java.util.Base64;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HostConfiguration;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.PutMethod;
@@ -18,9 +22,30 @@ import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
 
 public class UtilHttp {
 
+	private static boolean hasProxy;
+	
+	private static boolean hasCredentialProxy;
+	
+	private static String proxyHost;
+	private static int proxyPort;
+	private static String proxyUser;
+	private static String proxyPass;
+	
 	static {
 		Protocol easyhttps = new Protocol("https", (ProtocolSocketFactory)new EasySSLProtocolSocketFactory(), 443);
 		Protocol.registerProtocol("https", easyhttps);
+	}
+	
+	public static void setHttpProxy(String host, int port, String user, String pass) {
+		hasProxy = true;
+		proxyHost = host;
+		proxyPort = port;
+		
+		if(user != null && pass != null && !user.isEmpty()) {
+			proxyUser = user;
+			proxyPass = pass;
+			hasCredentialProxy = true;
+		}
 	}
 
 	public static String getAuthHeaderBase64(String clientId, String clientSecret) {
@@ -42,6 +67,7 @@ public class UtilHttp {
 					httpPost.addRequestHeader(entry.getKey(), entry.getValue());
 				}
 				HttpClient httpClient = new HttpClient();
+				configProxy(httpClient);
 				int result = httpClient.executeMethod(httpPost);
 
 				RespHttp resp = new RespHttp(result, httpPost);
@@ -73,6 +99,7 @@ public class UtilHttp {
 					httpPost.addRequestHeader(entry.getKey(), entry.getValue());
 				}
 				HttpClient httpClient = new HttpClient();
+				configProxy(httpClient);
 				int result = httpClient.executeMethod(httpPost);
 
 				RespHttp resp = new RespHttp(result, httpPost);
@@ -105,6 +132,8 @@ public class UtilHttp {
 				httpGet.addRequestHeader(entry.getKey(), entry.getValue());
 			}
 			HttpClient httpClient = new HttpClient();
+			configProxy(httpClient);
+
 			try {
 				int result = httpClient.executeMethod(httpGet);
 
@@ -116,6 +145,18 @@ public class UtilHttp {
 			}
 		} catch (Exception e) {
 			return new RespHttp(e);
+		}
+	}
+
+	private static void configProxy(HttpClient httpClient) {
+		if(hasProxy) {
+			HostConfiguration config = httpClient.getHostConfiguration();
+			config.setProxy(proxyHost, proxyPort);
+			if(hasCredentialProxy) {
+				Credentials credentials = new UsernamePasswordCredentials(proxyUser, proxyPass);
+				AuthScope authScope = new AuthScope(proxyHost, proxyPort);
+				httpClient.getState().setProxyCredentials(authScope, credentials);					
+			}
 		}
 	}
 
